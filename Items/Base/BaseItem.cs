2e377ed046ba4aa4ec581cbcfc299f15670a6a62
Michal-MK <michalhz159@gmail.com>
2021-03-02 10:12:06 +0100

    Initial Restructuring commit

---
+++ Items/Base/BaseItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+
+namespace MagicStorage.Items.Base {
+	internal class BaseItem {
+		public void SetStaticDefaults() {
+			//base.SetStaticDefaults();
+		}
+
+		public void SetDefaults() {
+			//base.SetDefaults();
+		}
+	}
+}
